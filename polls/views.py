82270202e58ef83b1c893ec7bcd945ff080ded62
YChaeun <se_same5315@naver.com>
2019-07-09 17:41:44 +0900

    edit view : Http404

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
+from django.http import Http404
 
 def index(request) :
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -9,7 +10,11 @@ def index(request) :
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-        return HttpResponse("question list %s" % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist!!!!!!!!!!!!!")
+    return render(request, 'polls/detail.html', {'question':question})
 
 def result(request, question_id):
     response = "result : %s"
