e6d6b9c221b172f91f518e44bd0a0361174cb21c
YChaeun <se_same5315@naver.com>
2019-07-10 14:33:10 +0900

    feature vote: edit view

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
 from django.template import loader
-from django.http import Http404
+from django.urls import reverse
 
 def index(request) :
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -18,4 +18,15 @@ def result(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse(f"vote for {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+                'question' : question,
+                'error_message' : "You didn't select a choice",
+            })
+    else :
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
